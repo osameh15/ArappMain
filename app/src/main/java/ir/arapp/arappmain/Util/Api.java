aefb7faf04cf105295b9fc19eacbed4b1068da1c
osameh15 <osirandoust@gmail.com>
2021-01-13 12:11:26 +0330

    - Add Retrofit Client

---
+++ app/src/main/java/ir/arapp/arappmain/Util/Api.java
@@ -0,0 +1,6 @@
+package ir.arapp.arappmain.Util;
+
+public interface Api
+{
+
+}
---
+++ app/src/main/java/ir/arapp/arappmain/Util/RetrofitClient.java
@@ -0,0 +1,40 @@
+package ir.arapp.arappmain.Util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient
+{
+    private static final String BASE_URL = "http://cafestudy.ir/public/";
+    private static RetrofitClient mInstance;
+    private final Retrofit retrofit;
+
+    private RetrofitClient()
+    {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        retrofit = new Retrofit
+                .Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
+
+    public static synchronized RetrofitClient getInstance()
+    {
+        if (mInstance == null)
+        {
+            mInstance = new RetrofitClient();
+        }
+        return mInstance;
+    }
+
+    public Api getApi()
+    {
+        return retrofit.create(Api.class);
+    }
+}
