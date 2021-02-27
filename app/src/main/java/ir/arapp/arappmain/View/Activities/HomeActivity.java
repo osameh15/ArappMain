1db10d7913dd2df68e148abef3483c71dcd6d17f
osameh15 <osirandoust@gmail.com>
2021-02-27 23:11:33 +0330

    - Design drawer layout

--- app/src/main/java/ir/arapp/arappmain/View/Activities/HomeActivity.java
+++ app/src/main/java/ir/arapp/arappmain/View/Activities/HomeActivity.java
@@ -1,7 +1,10 @@
 package ir.arapp.arappmain.View.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -13,6 +16,11 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -28,12 +36,15 @@ public class HomeActivity extends AppCompatActivity
 {
 
 //    region Variables
+    private static final float END_SCALE = 0.7f;
     ActivityHomeBinding activityHomeBinding;
     NavHostFragment navHostFragment;
     NavController navController;
     AppBarConfiguration appBarConfiguration;
     private SnackBarToast snackBarToast;
     private long backPressedTime;
+    private String versionName;
+    private SessionManager sessionManager;
 //    endregion
 
     @Override
@@ -44,6 +55,7 @@ public class HomeActivity extends AppCompatActivity
 //        Hooks
         navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragment);
         snackBarToast = new SnackBarToast(activityHomeBinding.getRoot());
+        sessionManager = new SessionManager(this);
 //        Action bar
         setSupportActionBar(activityHomeBinding.homeToolbar);
         Objects.requireNonNull(getSupportActionBar()).hide();
@@ -51,8 +63,13 @@ public class HomeActivity extends AppCompatActivity
         navController = navHostFragment.getNavController();
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        Navigation drawer manager
+        navigationDrawerManager();
 //        Bottom navigation Manager
         bottomBarManager();
+//        Check app version every time on lunch home screen
+        versionName = sessionManager.getVersionName(this, HomeActivity.class);
+        checkAppVersion();
     }
 
 //    region Methods
@@ -69,18 +86,25 @@ public class HomeActivity extends AppCompatActivity
     @Override
     public void onBackPressed()
     {
-        if (Objects.equals(Objects.requireNonNull(navController.getCurrentDestination()).getLabel(), "صفحه اصلی"))
+        if (activityHomeBinding.drawerLayout.isDrawerVisible(GravityCompat.END))
         {
-            snackBarToast.snackBarShortTime("جهت خروج از برنامه، بازگشت را مجددا فشار دهید!", activityHomeBinding.bottomNavigationView);
-            if (backPressedTime + 2000 > System.currentTimeMillis())
-            {
-                finishAffinity();
-            }
-            backPressedTime = System.currentTimeMillis();
+            activityHomeBinding.drawerLayout.closeDrawer(GravityCompat.END);
         }
         else
         {
-            super.onBackPressed();
+            if (Objects.equals(Objects.requireNonNull(navController.getCurrentDestination()).getLabel(), "صفحه اصلی"))
+            {
+                snackBarToast.snackBarShortTime("جهت خروج از برنامه، بازگشت را مجددا فشار دهید!", activityHomeBinding.bottomNavigationView);
+                if (backPressedTime + 2000 > System.currentTimeMillis())
+                {
+                    finishAffinity();
+                }
+                backPressedTime = System.currentTimeMillis();
+            }
+            else
+            {
+                super.onBackPressed();
+            }
         }
     }
     @Override
@@ -88,6 +112,87 @@ public class HomeActivity extends AppCompatActivity
     {
         navController.navigateUp();
         return true;
+    }
+//    Check application version in lunch app at home screen every time
+    private void checkAppVersion()
+    {
+        String storedVersionName = sessionManager.getStoredVersionName();
+        if (storedVersionName.equals(""))
+        {
+            sessionManager.setStoredVersionName(versionName);
+        }
+        else if (!storedVersionName.equals(versionName))
+        {
+//            Todo show recently update dialog
+        }
+        sessionManager.setStoredVersionName(versionName);
+    }
+//    Navigation Drawer Manager and animated drawer layout
+    private void navigationDrawerManager()
+    {
+        activityHomeBinding.navigationDrawer.bringToFront();
+        activityHomeBinding.navigationDrawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_faves)
+                {
+                    snackBarToast.snackBarShortTime("نشان شده ها", activityHomeBinding.bottomNavigationView);
+                }
+                else if (itemId == R.id.nav_newsReceive)
+                {
+                    snackBarToast.snackBarShortTime("دریافت اعلان", activityHomeBinding.bottomNavigationView);
+                }
+                else if (itemId == R.id.nav_inviteFriends)
+                {
+                    snackBarToast.snackBarShortTime("پیشنهاد به دوستان", activityHomeBinding.bottomNavigationView);
+                }
+                else if (itemId == R.id.nav_rating)
+                {
+                    snackBarToast.snackBarShortTime("امتیاز به آراپ", activityHomeBinding.bottomNavigationView);
+                }
+                else if (itemId == R.id.nav_recentUpdates)
+                {
+                    snackBarToast.snackBarShortTime("تغییرات اخیر", activityHomeBinding.bottomNavigationView);
+                }
+                else if (itemId == R.id.nav_aboutUs)
+                {
+                    snackBarToast.snackBarShortTime("درباره ما", activityHomeBinding.bottomNavigationView);
+                }
+                else if (itemId == R.id.nav_contactUS)
+                {
+                    snackBarToast.snackBarShortTime("ارتباط با ما", activityHomeBinding.bottomNavigationView);
+                }
+                else if (itemId == R.id.nav_question)
+                {
+                    snackBarToast.snackBarShortTime("سوالات متداول", activityHomeBinding.bottomNavigationView);
+                }
+                activityHomeBinding.drawerLayout.closeDrawer(GravityCompat.END);
+                return true;
+            }
+        });
+        animatedDrawerLayout();
+    }
+    private void animatedDrawerLayout()
+    {
+        activityHomeBinding.drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener()
+        {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset)
+            {
+//                 Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                activityHomeBinding.mainFragment.setScaleX(offsetScale);
+                activityHomeBinding.mainFragment.setScaleY(offsetScale);
+//                 Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = activityHomeBinding.mainFragment.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffsetDiff - xOffset;
+                activityHomeBinding.mainFragment.setTranslationX(xTranslation);
+            }
+        });
     }
     //    Bottom bar manager to handle bottom navigation
     private void bottomBarManager()
