fd8aee091be4b3bf9eef7b4b7ece04452e02a065
osameh15 <osirandoust@gmail.com>
2021-03-10 15:37:18 +0330

    - Edit User Profile xml

--- app/src/main/java/ir/arapp/arappmain/View/Fragments/EditUserFragment.java
+++ app/src/main/java/ir/arapp/arappmain/View/Fragments/EditUserFragment.java
@@ -2,11 +2,16 @@ package ir.arapp.arappmain.View.Fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import java.util.Objects;
 
 import ir.arapp.arappmain.Util.Services.NavigationManager;
 import ir.arapp.arappmain.databinding.FragmentEditUserBinding;
@@ -18,6 +23,12 @@ public class EditUserFragment extends Fragment
 //    endregion
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
 //        Inflate the layout for this fragment
@@ -25,10 +36,26 @@ public class EditUserFragment extends Fragment
 //        Drawer Locked and visible Bottom navigation
         ((NavigationManager) requireActivity()).setDrawerLocked(true);
         ((NavigationManager) requireActivity()).bottomNavigationVisibility(false);
+//        Toolbar
+        ((AppCompatActivity)requireActivity()).setSupportActionBar(fragmentEditUserBinding.profileToolbar);
+        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        fragmentEditUserBinding.profileToolbar.setNavigationOnClickListener(view1 -> onNavigateUp());
 //        Return View
         return fragmentEditUserBinding.getRoot();
     }
 
 //    region Methods
+//    Option menu and manage it
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+    }
+//    Back button navigation
+    private void onNavigateUp()
+{
+    requireActivity().onBackPressed();
+}
 //    endregion
 }
